02d3c5c336930c38b7288f9cd88093af8ff27303
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-12-13 21:53:39 +0100

    refactorized navigation with vuerouter

---
+++ mevn-app/client/src/routes/router.js
@@ -0,0 +1,62 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  { path: "/", redirect: "/dashboard" },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../components/Dashboard.vue"),
+  },
+  {
+    path: "/plan",
+    name: "Plan",
+    component: () => import("../components/MapPlan.vue"),
+  },
+  {
+    path: "/world",
+    name: "World",
+    component: () => import("../components/World.vue"),
+  },
+  {
+    path: "/trips",
+    name: "PastTrips",
+    component: () => import("../components/PastTrips.vue"),
+  },
+  {
+    path: "/live",
+    name: "Live",
+    component: () => import("../components/Live.vue"),
+  },
+  {
+    path: "/discover",
+    name: "Discover",
+    component: () => import("../components/Discover.vue"),
+  },
+  {
+    path: "/rewards",
+    name: "Rewards",
+    component: () => import("../components/Rewards.vue"),
+  },
+  {
+    path: "/tips",
+    name: "Tips",
+    component: () => import("../components/Tips.vue"),
+  },
+  {
+    path: "/feedback",
+    name: "Feedback",
+    component: () => import("../components/Feedback.vue"),
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: () => import("../components/Admin.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
