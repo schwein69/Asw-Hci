a3e100a594425b5ab08846276b3bcecb1995c83a
Jiahao Guo <jiahao.guo@studio.unibo.it>
2025-12-18 09:51:18 +0100

    fixed api

--- mevn-app/server/src/services/gemini.js
+++ mevn-app/server/src/services/gemini.js
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const API_KEY = "AIzaSyARZU0qXWXIPVNAcYzQ8IvuPZo7Bzmn5Y4";
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
