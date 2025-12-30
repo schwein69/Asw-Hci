adcbc6c9f198b6a1db0d11d9e0f5f50ca63c4c18
Intissar99 <meddebintissar@gmail.com>
2025-12-30 02:19:22 +0100

    Complete Backend: User registration and login with Bcrypt and JWT

--- mevn-app/server/src/routes/index.js
+++ mevn-app/server/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import tipsRoutes from "./tipsRouter.js";
+import userRoutes from "./userRouter.js";
 const router = express.Router();
 
 // Combine all route modules
 router.use("/tips", tipsRoutes);
+router.use("/users", userRoutes);
 
 export default router;
