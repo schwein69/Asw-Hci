ec7b5f05349e94bf8c0e2fb548a7deda384d2475
Jiahao Guo <jiahao.guo@studio.unibo.it>
2026-01-04 19:02:18 +0100

    added route and controller for plan simulation

--- mevn-app/server/src/routes/index.js
+++ mevn-app/server/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import tipsRoutes from "./tipsRouter.js";
+import planRoutes from "./planRouter.js";
 const router = express.Router();
 
 // Combine all route modules
 router.use("/tips", tipsRoutes);
+router.use("/plan", planRoutes);
 
 export default router;
