01155a176da9040fb3bb34cd5ae9b9f8cefe01f2
Jiahao Guo <jiahao.guo@studio.unibo.it>
2026-01-07 20:02:21 +0100

    better prompt for more precise estimation

--- mevn-app/server/src/routes/planRouter.js
+++ mevn-app/server/src/routes/planRouter.js
@@ -2,6 +2,6 @@ import express from "express";
 import * as planController from "../controllers/planController.js";
 const router = express.Router();
 
-router.get("/estimate", planController.getTravelItineraryEstimation);
+router.post("/estimate", planController.getTravelItineraryEstimation);
 
 export default router;
