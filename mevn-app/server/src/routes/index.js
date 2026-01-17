c221ee6df16f7a0cca95b447f08c0f167d740fb1
Horri kalile <103006189+Horri-kalile@users.noreply.github.com>
2026-01-18 13:04:15 +1400

    Register notification routes in main router

--- mevn-app/server/src/routes/index.js
+++ mevn-app/server/src/routes/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import tipsRoutes from "./tipsRouter.js";
 import planRoutes from "./planRouter.js";
 import userRoutes from "./userRouter.js";
+import notificationRoutes from "./notificationRouter.js";
 
 const router = express.Router();
 
 router.use("/tips", tipsRoutes);
 router.use("/plan", planRoutes);
 router.use("/users", userRoutes);
+router.use("/notifications", notificationRoutes);
 
 export default router;
