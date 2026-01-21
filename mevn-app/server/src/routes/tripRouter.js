77ac8ad0d955949987698ccd599ed09f5b59b8fd
Intissar99 <meddebintissar@gmail.com>
2026-01-21 22:54:32 +0100

    Add dashboard api

--- mevn-app/server/src/routes/tripRouter.js
+++ mevn-app/server/src/routes/tripRouter.js
@@ -3,12 +3,13 @@ import {
   getUserTrips,
   getUpcomingTrips,
   getActiveTrips,
-  getCompletedTrips,
+  getCompletedTripsAuth,
   getTripById,
   updateTrip,
   deleteTrip,
   markTripCompleted,
 } from "../controllers/tripController.js";
+import { authenticate } from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -21,8 +22,8 @@ router.get("/upcoming/:userId", getUpcomingTrips);
 // Get active/ongoing trips
 router.get("/active/:userId", getActiveTrips);
 
-// Get completed trips
-router.get("/completed/:userId", getCompletedTrips);
+// Get completed trips for current user (auth)
+router.get("/completed", authenticate, getCompletedTripsAuth);
 
 // Get single trip by ID
 router.get("/:tripId", getTripById);
