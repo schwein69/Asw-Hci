e27bfb386b1dc089f3c5ccd6e575ace92b574df3
Jiahao Guo <jiahao.guo@studio.unibo.it>
2026-02-04 20:45:00 +0100

    fix and merge

--- mevn-app/server/index.js
+++ mevn-app/server/index.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Make io accessible to routes
